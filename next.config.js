475297e3e460d779730cae2178e843638e925ffb
videsh <videsh.ghodarop@iaugmentor.com>
2021-05-12 09:16:43 +0530

    image optimazation p

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   target: 'serverless',
   images: {
-    // loader: 'cloudinary',
-    // path: 'https://res.cloudinary.com/dfygomb1s/image/fetch/https://ecommerce-videsh.herokuapp.com/',
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/dfygomb1s/image/fetch/',
     domains: ['ecommerce-videsh.herokuapp.com'],
   },
 };
