f22040c1aa98808fbd01e713fd06647a8b121d54
videsh <videsh.ghodarop@iaugmentor.com>
2021-05-20 13:27:26 +0530

    some changes in config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  target: 'serverless',
   images: {
-    loader: 'cloudinary',
-    path: 'https://res.cloudinary.com/dfygomb1s/image/fetch/',
     domains: ['ecommerce-videsh.herokuapp.com'],
   },
 };
